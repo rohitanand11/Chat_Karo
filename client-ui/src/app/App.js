bdb996211943018d54f569649582e0f7a13d3a26
Rohit Anand <rohitanand3456@gmail.com>
2021-02-04 10:35:51 +0530

    implemented a basic layout for the chat app.

--- client-ui/src/app/App.js
+++ client-ui/src/app/App.js
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from 'react';
 
 //importing local components
-import InitialModal from "./components/initialModal/InitialModal";
+import InitialModal from "../components/initialModal/InitialModal";
 
 //importing css and utility files
-import './App.css';
-import { socket } from "./utility/chatLogic/chatLogic";
+import Classes from './App.module.scss';
+// import { socket } from "./utility/chatLogic/chatLogic";
 
 function App() {
 
@@ -14,30 +14,30 @@ function App() {
   const [chatLogArray, setChatLogArray] = useState([]);
   const [showModal, setShowModal] = useState(true);
 
-  useEffect(() => {
-    socket.on('message', (msg) => {
-      console.log(msg);
-    });
-  }, []);
+  // useEffect(() => {
+  //   socket.on('message', (msg) => {
+  //     console.log(msg);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    socket.on('new_chat_message', (msg) => {
-      console.log(msg);
-      setChatLogArray((chatArray) => [...chatArray, msg]);
-    });
-  }, []);
+  // useEffect(() => {
+  //   socket.on('new_chat_message', (msg) => {
+  //     console.log(msg);
+  //     setChatLogArray((chatArray) => [...chatArray, msg]);
+  //   });
+  // }, []);
 
   const handleChangeInput = (evt) => {
     // console.log(evt.target.value);
     setChatInput(evt.target.value);
   }
 
-  const handleSend = () => {
-    if (chatInput.length > 0) {
-      socket.emit('chat_message', chatInput);
-      setChatInput("");
-    }
-  }
+  // const handleSend = () => {
+  //   if (chatInput.length > 0) {
+  //     socket.emit('chat_message', chatInput);
+  //     setChatInput("");
+  //   }
+  // }
 
   const handleModal = (pStatus) => {
     setShowModal(pStatus);
@@ -56,19 +56,29 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {
-        (showModal===true)
-          ? <InitialModal handleModal={handleModal}/>
-          : null
-      } 
-      <div className="textInput">
+    <div className={Classes.App}>
+      {(showModal===true) ? <InitialModal handleModal={handleModal}/> : null} 
+
+      <div className={Classes.appWindow}>
+        <div className ={Classes.aside}>
+
+        </div>
+        <div className = {Classes.chat_window}>
+
+        </div>
+      </div>
+
+
+
+
+
+      {/* <div className="textInput">
         <input className="chatInput" value={chatInput} onChange={handleChangeInput} />
         <button onClick={handleSend}>send </button>
       </div>
       <div className="texts">
         {renderChatLog()}
-      </div>
+      </div> */}
     </div>
   );
 }
